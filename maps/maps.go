3a5d441f0bb846bd7c72bd2f1dac5fb6562053b9
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2023-07-06 22:19:17 -0400

    add map module

--- maps/maps.go
+++ maps/maps.go
@@ -4,7 +4,11 @@ import "errors"
 
 type Dictionary map[string]string
 
-var ErrorNotFound = errors.New("could not find the word you were looking for")
+var (
+	ErrorNotFound         = errors.New("could not find the word you were looking for")
+	ErrorWordExists       = errors.New("cannot add word because it already exists")
+	ErrorWordDoesNotExist = errors.New("cannot update word because it does not exist")
+)
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
@@ -12,4 +16,36 @@ func (d Dictionary) Search(word string) (string, error) {
 		return "", ErrorNotFound
 	}
 	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrorNotFound:
+		d[word] = definition
+	case nil:
+		return ErrorWordExists
+	default:
+		return err
+
+	}
+	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case ErrorNotFound:
+		return ErrorWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+	return nil
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
 }
--- maps/maps_test.go
+++ maps/maps_test.go
@@ -26,4 +26,64 @@ func TestSearch(t *testing.T) {
 			t.Errorf("got %q want %q", err, want)
 		}
 	})
+
+	t.Run("add word that doesn't exists", func(t *testing.T) {
+		dictionary.Add("test2", "this is just a test2")
+		got, _ := dictionary.Search("test2")
+		want := "this is just a test2"
+
+		if got != want {
+			t.Errorf("got %q want %q given, %q", got, want, "test2")
+		}
+	})
+
+	t.Run("add word that already exists", func(t *testing.T) {
+		err := dictionary.Add("test", "this is just a test")
+		want := ErrorWordExists
+
+		if err == nil {
+			t.Fatal("expected to get an error.")
+		}
+
+		if err != want {
+			t.Errorf("got %q want %q", err, want)
+		}
+	})
+
+	t.Run("update word that exists", func(t *testing.T) {
+		dictionary.Update("test", "this is just a test updated")
+		got, _ := dictionary.Search("test")
+		want := "this is just a test updated"
+
+		if got != want {
+			t.Errorf("got %q want %q given, %q", got, want, "test")
+		}
+	})
+
+	t.Run("update word that doesn't exists", func(t *testing.T) {
+		err := dictionary.Update("test3", "this is just a test3")
+		want := ErrorWordDoesNotExist
+
+		if err == nil {
+			t.Fatal("expected to get an error.")
+		}
+
+		if err != want {
+			t.Errorf("got %q want %q", err, want)
+		}
+	})
+
+	t.Run("delete word that exists", func(t *testing.T) {
+		dictionary.Delete("test")
+		_, err := dictionary.Search("test")
+		want := ErrorNotFound
+
+		if err == nil {
+			t.Fatal("expected to get an error.")
+		}
+
+		if err != want {
+			t.Errorf("got %q want %q", err, want)
+		}
+	})
 }
